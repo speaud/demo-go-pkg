f9d39729aa59fe9a4756b1d8b1eae7e8bb8fb389
Joshua Mummert <jm@joshuamummert.com>
2021-12-18 15:39:30 -0500

    Add param

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
-func helloworld() string {
-	return "Hello World!!"
+func helloworld(b string) string {
+
+	return "Hello World!!" + b
 }
 
 func main() {
-	fmt.Println(helloworld())
+	fmt.Println(helloworld("asd"))
 }
+
+/*
+
+docker run --rm golang:1.5
+docker run -it --rm golang
+
+docker run --rm golang sh -c "go get https://github.com/speaud/demo-go-pkg && exec demo-go-pkg"
+
+go get <path-to-repo>@<branch>
+
+
+*/
