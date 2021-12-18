3d2887856dadb2b9bdfa5c45adbd3ee1f20dff97
Joshua Mummert <jm@joshuamummert.com>
2021-12-18 15:40:53 -0500

    Fix test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	if helloworld() != "Hello World!!" {
+	if helloworld("asd") != "Hello World!!asd" {
 		t.Fatal("Test fail")
 	}
 }
